a2c55285419f9aad94815c5e2e6c87a2052e1996
kaviraj121 <kavir916@gmail.com>
2025-03-18 23:59:48 +0530

    chages made lets deploy

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/workflows(.*)",
-  "/api/webhooks/stripe/route(.*)",
+  "/api/webhooks/stripe/(.*)",
   "/",
 ]);
 
