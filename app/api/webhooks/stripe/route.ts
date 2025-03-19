519c3d906f7c3bcc66d77d3db2528b87656ea3a8
kaviraj121 <kavir916@gmail.com>
2025-03-19 10:00:23 +0530

    made changes in route

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,35 +1,29 @@
 import { handleCheckoutSessionCompleted } from "@/lib/stripe/handleCheckoutSessionCompleted";
 import { stripe } from "@/lib/stripe/stripe";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-
 export async function POST(request: Request) {
-    const body = await request.text();
-  
-    const signatureHeaders = headers().get("stripe-signature") as string;
-  
-    try {
-      const event = stripe.webhooks.constructEvent(
-        body,
-        signatureHeaders,
-        process.env.STRIPE_WEBHOOK_SECRET!
-      );
+  const body = await request.arrayBuffer(); // Read raw body as a buffer
+  const signature = request.headers.get("stripe-signature") as string;
 
-      
-  
-      switch (event.type) {
-        case "checkout.session.completed":
-          handleCheckoutSessionCompleted(event.data.object);
-          break;
-  
-        default:
-          break;
-      }
-  
-      return new NextResponse(null, { status: 200 });
-    } catch (error) {
-      console.error("Stripe webhook error", error);
-      return new NextResponse("webhook error", { status: 400 });
+  try {
+    const event = stripe.webhooks.constructEvent(
+      Buffer.from(body), // Convert buffer to string
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+
+    switch (event.type) {
+      case "checkout.session.completed":
+        await handleCheckoutSessionCompleted(event.data.object);
+        break;
+      default:
+        break;
     }
+
+    return new NextResponse(null, { status: 200 });
+  } catch (error) {
+    console.error("Stripe webhook error", error);
+    return new NextResponse("Webhook error", { status: 400 });
   }
+}
